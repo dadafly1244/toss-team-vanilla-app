9105259d5b99d1279166374f8cf8070bdb8c51af
YANGDAYOUNG <jc1525@hanmail.net>
2022-04-23 23:47:05 +0900

    feat: add font
    폰트추가
    해더 색 변화 1단계 성공..1!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
 // 배너 스크롤시 불투명으로 바뀌는 효과
 const headerEl = document.querySelector('header');
+const logoWhightEl = document.querySelector('.logo .white')
+const logoBlueEl = document.querySelector('.logo .blue')
+const imgInvisible = document.querySelector('.invisible');
+
+
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
-  if (window.scrollY > 5){
-    headerEl.classList.add('filter');
-  }else{
+  if (window.scrollY < 5){
     headerEl.classList.remove('filter');
-  }
+    changeLogoToBlueColor(false)
+
+  } else if (window.scrollY < 3600 && window.scrollY >=5) {
+    headerEl.classList.add('filter');
+    changeLogoToBlueColor(false)
+    
+  } else {
+    headerEl.classList.remove('filter');
+    changeLogoToBlueColor(true)
+
+  } 
+
   
 },300));
 
+
+function changeLogoToBlueColor(isBlue) {
+  if (isBlue) {
+    //로고 파란색으로 
+    logoBlueEl.classList.remove('invisible')
+    logoWhightEl.classList.add('invisible')
+  }
+  else{
+    //로고 흰색으로
+    logoWhightEl.classList.remove('invisible')
+    logoBlueEl.classList.add('invisible')
+  }
+}
