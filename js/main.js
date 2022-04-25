c89c99ca7745847f19f0a8158715105372618e55
YANGDAYOUNG <jc1525@hanmail.net>
2022-04-26 00:30:26 +0900

    Refactor&Design:hearder color change when scroll
    일정 수준 이상 스크롤을 내리면 해더의 로고, 배경색, 글자색이 변하는 기능
    코드 정리

--- js/main.js
+++ js/main.js
@@ -1,64 +1,44 @@
-
+// HEADER
 const headerEl = document.querySelector('header');
 const logoWhightEl = document.querySelector('.logo .white')
 const logoBlueEl = document.querySelector('.logo .blue')
 const imgInvisible = document.querySelector('.invisible');
 const headerMenuEls = headerEl.querySelectorAll('a')
-console.log(headerMenuEls)
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
   if (window.scrollY < 5){
-    // 배너 스크롤시 불투명으로 바뀌는 효과
     headerEl.classList.remove('filter');
-    headerEl.classList.remove('bg--white')
-    changeLogoToBlueColor(false)
-    addBackgroundClass(false)
-
+    changeHeaderColor(false)
   } else if (window.scrollY < 3600 && window.scrollY >=5) { 
-    // 스크롤5일때 뒷 배경 블러 처러하기. 
+    //배너 스크롤시 불투명으로 바뀌는 효과
     headerEl.classList.add('filter');
-    headerEl.classList.remove('bg--white')
-    changeLogoToBlueColor(false)
-    addBackgroundClass(false)
-
-    
+    changeHeaderColor(false)
   } else {
     headerEl.classList.remove('filter');
     //timeline 이후부터 로고 색을 파란색으로 변경하기. 
-    changeLogoToBlueColor(true)
-    headerEl.classList.add('bg--white')
-    addBackgroundClass(true)
-
-
+    changeHeaderColor(true)
   } 
-
-  
 },300));
 
-
-function changeLogoToBlueColor(isBlue) {
-  if (isBlue) {
+function changeHeaderColor(isWhiteBackground){ //header 색 바꾸는 함수
+  if(isWhiteBackground){
     //로고 파란색으로 
     logoBlueEl.classList.remove('invisible')
     logoWhightEl.classList.add('invisible')
-  }
-  else{
+    //배경색을 흰색으로 
+    headerEl.classList.add('bg--white')
+    //해더의 매뉴에 글자 색을 검은색으로 만드는 클래스 넣기 
+    headerMenuEls.forEach( item => {item.classList.add('bg--white')})
+  }else{
     //로고 흰색으로
     logoWhightEl.classList.remove('invisible')
     logoBlueEl.classList.add('invisible')
+    //배경 흰색 제거
+    headerEl.classList.remove('bg--white')
+    headerMenuEls.forEach( item => {item.classList.remove('bg--white')})
   }
 }
 
-function addBackgroundClass(isWhiteBackground) {
-  //해더의 매뉴에 글자 색을 검은색으로 만드는 클래스 넣기 
-  if(isWhiteBackground){
-    headerMenuEls.forEach( item => {
-      item.classList.add('bg--white')
-    })
-  }else { 
-    headerMenuEls.forEach( item => {
-      item.classList.remove('bg--white')
-    })
-  }
-}
+
+
